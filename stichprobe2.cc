e689b6d0e8ea6300a727f2e206f8f4867f3c160a
repl.it user <replituser@example.com>
2020-11-18 16:28:44 +0000

    Versuch1

--- stichprobe2.cc
+++ stichprobe2.cc
@@ -7,6 +7,10 @@ int main(){
   std::ifstream fin2("datensumme.txt");
   std::ofstream fout("mittelwerte.txt");
   std::ofstream fout2("varianzen.txt");
+
+  float summean;
+  float sumvar;
+
   for (int j = 0; j < 26; ++j){
     float sum = 0;
     for (int i = j * 9; i < (j + 1) * 9; ++i){
@@ -14,8 +18,8 @@ int main(){
       fin >> zahl;
       sum = sum + zahl;
     }
-    float mean = sum / 9;
-
+  float mean = sum / 9;
+   summean = summean + mean;
   float var=0;
   for (int i = j * 9; i < (j + 1) * 9; ++i){
       int zahl;
@@ -31,4 +35,28 @@ int main(){
   fin2.close();
   fout.close();
   fout2.close();
+
+  std::ifstream fin4("mittelwerte.txt"); 
+  std::ifstream fin3("varianzen.txt");
+ 
+  float sum = 0;
+  float sumv = 0;
+  for (int i = 0; i < 26; ++i){
+    float zahl2;
+    float zahl3;
+
+    fin4 >> zahl2;
+    fin3 >> zahl3;
+
+    sum = sum + zahl2;  
+    sumv = sumv + zahl3;
+  }
+  float mean = sum / 26.0;
+  float meanv = sumv / 26.0;
+  
+  std::cout << "mean mean   " << mean << std::endl;
+  std::cout << "mean var   " << meanv << std::endl;
+
+  fin4.close();
+  fin3.close();
 }
